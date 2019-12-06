a4c5213e8b24ac5441d201494462373df577c113
Konstantin Mokry <konstantin_mokry@epam.com>
2019-12-06 18:23:55 +0200

    fix deploy bugs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/details-and-service'));
+app.use(express.static('./dist/details-and-service-app'));
 
 app.get('/*', function(req,res) {
 
